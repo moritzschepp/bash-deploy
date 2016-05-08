ddba2b1f3b0c5e71e0c00304d6bd029bd560e46d
Moritz Schepp <moritz.schepp@gmail.com>
2016-05-09 00:00:29 +0200

    finalized separation of app specifics, deploy code and config

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ if [ -d deploy.sh ]; then
   cp $CALL_ROOT/lib.sh > deploy/lib.sh
 else
   mkdir -p deploy
-  cp $CALL_ROOT/lib.sh > deploy/lib.sh
-  cp $CALL_ROOT/app.sh.example > deploy/app.sh
+  cp $CALL_ROOT/lib.sh deploy/lib.sh
+  cp $CALL_ROOT/app.sh.example deploy/app.sh
   cp $CALL_ROOT/config.sh.example deploy/config.sh
   chmod +x deploy/app.sh
+
+  echo "The deployment scripts have been installed to the deploy/ directory."
+  echo "Run this script again to update the scripts while keeping your"
+  echo "configuration. Make sure to put deploy/config.sh in your gitignore."
 fi
