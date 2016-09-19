34946b5fec24c9b813711b61db84a4d35eb22e3d
Moritz Schepp <moritz.schepp@gmail.com>
2016-09-19 16:25:38 +0200

    small improvements

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 CALL_ROOT="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ -d deploy.sh ]; then
+if [ -d deploy ]; then
   # this is an update
-  cp $CALL_ROOT/lib.sh > deploy/lib.sh
+  cp $CALL_ROOT/lib.sh deploy/lib.sh
 else
   mkdir -p deploy
   cp $CALL_ROOT/lib.sh deploy/lib.sh
@@ -14,5 +14,6 @@ else
 
   echo "The deployment scripts have been installed to the deploy/ directory."
   echo "Run this script again to update the scripts while keeping your"
-  echo "configuration. Make sure to put deploy/config.sh in your gitignore."
+  echo "configuration. Make sure to put deploy/config.sh in your gitignore. Run"
+  echo "deploy/app.sh to deploy."
 fi
